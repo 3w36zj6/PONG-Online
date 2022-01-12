d59823b858295c93a4d8626a79b6e4dd64cf6349
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-01-13 05:59:45 +0900

    待機部屋

--- client/client.py
+++ client/client.py
@@ -17,6 +17,10 @@ class MyCustomNamespace(socketio.ClientNamespace):
     def on_response(self, msg):
         print(f"[{datetime.now().strftime(YMDHMS)}] response : {msg}")
 
+    def on_enter_room(self, msg):
+        print(f"[{datetime.now().strftime(YMDHMS)}] enter : {msg}")
+        App.game_mode = 2
+
 
 class SocketIOClient:
     def __init__(self, host, path):
@@ -31,6 +35,9 @@ class SocketIOClient:
 
     def disconnect(self):
         self.client.disconnect()
+
+    def get_sid(self):
+        return self.client.get_sid(namespace=self.path)
 
     def test(self, message):
         self.client.emit("broadcast_message", message, namespace=self.path)
@@ -53,10 +60,15 @@ class Bat:
 
 
 class App:
+    game_mode: int = 0
+
     def __init__(self, host):
         pyxel.init(640, 360, title="Hello Pyxel")
 
+        App.game_mode = 1
+
         self.socketio_client = SocketIOClient(host=host, path="/test")
+
         self.bat = Bat(x=10, y=10)
 
         pyxel.run(self.update, self.draw)
@@ -65,8 +77,16 @@ class App:
 
         self.bat.update()
 
-        # socketio
-        self.socketio_client.test({"x": self.bat.x, "y": self.bat.y})
+        if App.game_mode == 0:  # タイトル
+            pass
+        elif App.game_mode == 1:  # 待機部屋
+            pass
+        elif App.game_mode == 2:  # 対戦中
+            pass
+        elif App.game_mode == 3:  # 対戦終了
+            pass
+
+        # self.socketio_client.test({"x": self.bat.x, "y": self.bat.y})
 
         # exit
         if pyxel.btn(pyxel.KEY_Q):
@@ -76,6 +96,15 @@ class App:
     def draw(self):
         pyxel.cls(0)
 
+        pyxel.text(0, 0, f"{self.socketio_client.get_sid()}", 3)
+        if App.game_mode == 0:  # タイトル
+            pass
+        elif App.game_mode == 1:  # 待機部屋
+            pyxel.text(40, 40, f"waiting room", 7)
+        elif App.game_mode == 2:  # 対戦中
+            pass
+        elif App.game_mode == 3:  # 対戦終了
+            pass
         self.bat.draw()
 
 
--- server/server.py
+++ server/server.py
@@ -5,10 +5,30 @@ from datetime import datetime
 YMDHMS = "%Y-%m-%d %H:%M:%S"
 
 
+class Player:
+    players: list["Player"] = []
+
+    @classmethod
+    def setup(cls):
+        cls.players = []
+
+    @classmethod
+    def append(cls, sid):  # 待機室に追加
+        cls.players.append(sid)
+
+
 class MyCustomNamespace(socketio.Namespace):
     def on_connect(self, sid, environ):
         print(f"[{datetime.now().strftime(YMDHMS)}] connect sid : {sid}")
         print(f"[{datetime.now().strftime(YMDHMS)}] connect env : {environ}")
+
+        if not Player.players:
+            Player.append(sid)
+        else:
+            sid2 = Player.players.pop()
+            self.enter_room(sid, sid + sid2)
+            self.enter_room(sid2, sid + sid2)
+            self.emit("enter_room", sid + sid2, room=sid + sid2)
 
     def on_sid_message(self, sid, msg):  # 送信してきたクライアントだけにメッセージを送る
         self.emit("response", msg, room=sid)
@@ -21,8 +41,13 @@ class MyCustomNamespace(socketio.Namespace):
     def on_disconnect(self, sid):
         print(f"[{datetime.now().strftime(YMDHMS)}] disconnect sid : {sid}")
 
+        if sid in Player.players:
+            Player.players.remove(sid)
+
 
 if __name__ == "__main__":
+    Player.setup()
+
     server = socketio.Server(cors_allowed_origins="*")
     server.register_namespace(MyCustomNamespace("/test"))
     app = socketio.WSGIApp(server)
